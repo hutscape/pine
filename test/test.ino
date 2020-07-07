3d6ec6bf5201357eb260ad43e708fd4a26ebf7c2
Sayanee <sayanee@gmail.com>
2020-07-07 17:12:06 +0800

    test: add test code for receiving IR signals

--- test/test.ino
+++ test/test.ino
@@ -1,22 +1,31 @@
 #include "Adafruit_Si7021.h"
 #include <IRLibSendBase.h>
 #include <IRLib_HashRaw.h>
+#include <IRLibRecvPCI.h>
 #include <WebUSB.h>
-
 #include "./data.h"
 
 #define LED 13
+// https://github.com/cyborg5/IRLib2/blob/master/IRLibProtocols/IRLibSAMD21.h#L38-L39
+// For SAMD21 boards: We are recommending using Pin 5 for receiving
+#define IR_RECEIVE_PIN 5
+
+// https://github.com/cyborg5/IRLib2/blob/master/IRLibProtocols/IRLibSAMD21.h#L40-L41
+// For SAMD21 boards: For sending, default pin is 9
+#define IR_EMIT_PIN 9
 
 Adafruit_Si7021 sensor = Adafruit_Si7021();
 WebUSB WebUSBSerial(1, "webusb.github.io/arduino/demos/console");
+IRrecvPCI myReceiver(IR_RECEIVE_PIN);
 IRsendRaw mySender;
 
 void setup() {
-  pinMode(LED, OUTPUT);
-
   SerialUSB.begin(9600);
   while (!SerialUSB) {}
   delay(1000);
+
+  pinMode(LED, OUTPUT);
+  myReceiver.enableIRIn();
 
   SerialUSB.println("Starting Pine design verification test!");
   SerialUSB.println("-------------------------------------");
@@ -47,28 +56,16 @@ void setup() {
   SerialUSB.println("Sent Turn OFF Aircon");
   delay(2000);
 
-  SerialUSB.println("\n\nTest 5: It expects to send message to the browser");
-  WebUSBSerial.write("Hello world to the browser!");
-  WebUSBSerial.flush();
-  delay(1000);
-
-  WebUSBSerial.begin(9600);
-  // while (!WebUSBSerial) {}
-  delay(1000);
-  SerialUSB.println("Started Web USB");
-
-  SerialUSB.println("\n\nTest 6: It expects to send message from the browser");
-  delay(1000);
-
-  SerialUSB.println("\n\nTest 8: It expects to go to sleep and wake up");
-  delay(1000);
+  SerialUSB.println("\n\nTest 5: It expects to receive IR signals");
+  delay(2000);
 }
 
 void loop() {
-  WebUSBSerial.write("Hello world to the browser!");
-  WebUSBSerial.flush();
-
-  delay(2000);
+  if (myReceiver.getResults()) {
+    SerialUSB.print("Received IR signal length: ");
+    SerialUSB.println(recvGlobal.recvLength, DEC);
+    myReceiver.enableIRIn();
+  }
 }
 
 void blink(int num) {
