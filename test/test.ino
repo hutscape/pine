e1bbd8bd59bd1bc29f5b37638e9f17738ba91270
Sayanee <sayanee@gmail.com>
2020-07-09 16:48:09 +0800

    test: add test firmware

--- test/test.ino
+++ test/test.ino
@@ -18,10 +18,15 @@ Adafruit_Si7021 sensor = Adafruit_Si7021();
 WebUSB WebUSBSerial(1, "webusb.github.io/arduino/demos/console");
 IRrecvPCI myReceiver(IR_RECEIVE_PIN);
 IRsendRaw mySender;
+String readString;
+int test = 0;
 
 void setup() {
   SerialUSB.begin(9600);
   while (!SerialUSB) {}
+
+  WebUSBSerial.begin(9600);
+
   delay(1000);
 
   pinMode(LED, OUTPUT);
@@ -47,26 +52,132 @@ void setup() {
   delay(1000);
 
   SerialUSB.println("\n\nTest 3: It expects to turn ON the aircon");
-  mySender.send(rawDataON, RAW_DATA_LEN, 38);
-  SerialUSB.println("Sent Turn ON Aircon");
-  delay(2000);
+  SerialUSB.print("Have you pointed the circuit to the aircon? [y/n]: ");
+  test = 0;
+  while (test == 0) {
+    while (SerialUSB.available()) {
+      delay(3);
+      char c = SerialUSB.read();
+      readString += c;
+    }
+
+    if (readString == "y") {
+      SerialUSB.println(readString);
+      mySender.send(rawDataON, RAW_DATA_LEN, 38);
+      SerialUSB.println("Sent Turn ON Aircon");
+
+      readString = "";
+      test = 1;
+    }
+  }
+  delay(1000);
 
   SerialUSB.println("\n\nTest 4: It expects to turn OFF the aircon");
-  mySender.send(rawDataOFF, RAW_DATA_LEN, 38);
-  SerialUSB.println("Sent Turn OFF Aircon");
-  delay(2000);
+  SerialUSB.print("Have you pointed the circuit to the aircon? [y/n]: ");
+  test = 0;
+  while (test == 0) {
+    while (SerialUSB.available()) {
+      delay(3);
+      char c = SerialUSB.read();
+      readString += c;
+    }
+
+    if (readString == "y") {
+      SerialUSB.println(readString);
+      mySender.send(rawDataOFF, RAW_DATA_LEN, 38);
+      SerialUSB.println("Sent Turn OFF Aircon");
+
+      readString = "";
+      test = 1;
+    }
+  }
+  delay(1000);
 
   SerialUSB.println("\n\nTest 5: It expects to receive IR signals");
-  delay(2000);
+  SerialUSB.print("Have you pressed and pointed the remote control to Pine? [y/n]: ");
+  test = 0;
+  while (test == 0) {
+    while (SerialUSB.available()) {
+      delay(3);
+      char c = SerialUSB.read();
+      readString += c;
+    }
+
+    if (myReceiver.getResults()) {
+      SerialUSB.print(" Received IR signal length: ");
+      SerialUSB.print(recvGlobal.recvLength, DEC);
+      SerialUSB.print("     ");
+      myReceiver.enableIRIn();
+    }
+
+    if (readString == "y") {
+      SerialUSB.println(readString);
+      readString = "";
+      test = 1;
+    }
+  }
+
+  SerialUSB.println("\n\nTest 6: It expects to receive info from the browser");
+  SerialUSB.print("Have you turned on/off led from hutscape.com/webusb-led? [y/n]: ");
+  test = 0;
+  while (test == 0) {
+    while (SerialUSB.available()) {
+      delay(3);
+      char c = SerialUSB.read();
+      readString += c;
+    }
+
+    if (readString == "y") {
+      SerialUSB.println(readString);
+      readString = "";
+      test = 1;
+    }
+
+    if (WebUSBSerial && WebUSBSerial.available()) {
+      int byte = WebUSBSerial.read();
+
+      if (byte == 'H') {
+        SerialUSB.print("LED ON     ");
+        WebUSBSerial.write("Turning LED on.");
+        digitalWrite(LED, HIGH);
+      } else if (byte == 'L') {
+        SerialUSB.print("LED OFF     ");
+        WebUSBSerial.write("Turning LED off.");
+        digitalWrite(LED, LOW);
+      }
+
+      WebUSBSerial.flush();
+    }
+  }
+  delay(1000);
+
+  SerialUSB.println("\n\nTest 7: It expects to send info to the browser");
+  SerialUSB.print("Have you open the dev console at hutscape.com/webusb-send? [y/n]: ");
+  test = 0;
+  while (test == 0) {
+    while (SerialUSB.available()) {
+      delay(3);
+      char c = SerialUSB.read();
+      readString += c;
+    }
+
+    if (readString == "y") {
+      SerialUSB.println(readString);
+      readString = "";
+      test = 1;
+
+      WebUSBSerial.write("Hello from mcu to the browser!");
+      WebUSBSerial.flush();
+    }
+  }
+  delay(1000);
+
+  SerialUSB.println("\n\n-------------------------------------");
+  SerialUSB.println("Completed Pine design verification test!");
+  SerialUSB.println("-------------------------------------");
 }
 
-void loop() {
-  if (myReceiver.getResults()) {
-    SerialUSB.print("Received IR signal length: ");
-    SerialUSB.println(recvGlobal.recvLength, DEC);
-    myReceiver.enableIRIn();
-  }
-}
+void loop() { }
 
 void blink(int num) {
   for (int i=0; i < num; i++) {
